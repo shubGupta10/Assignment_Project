dba9dad266bce88d180d25325cdc3ae8aef999e2
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-03 00:25:04 +0530

    Added listening PORT

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,13 @@
 import connect from "./db/db.js";
 import 'dotenv/config';
 
-connect();
+connect()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is running at ${process.env.PORT}`);
+    })
+})
+  .catch((err) => {
+    console.log("MongoDB connection failed.", err);
+    process.exit(1);
+});
