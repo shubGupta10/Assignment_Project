4d8ea3a39aabe3b39a95eefeb109202b23b30a73
Shubham Kumar Gupta <shubhamkumargupta1012@gmail.com>
2024-04-08 11:30:36 +0530

    Bugs fixes

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,12 @@
 import  express  from "express";
 import cors from "cors"
-// import profileRoutes from "./routes/profileRoutes.js"
 import profileRoutes from "./routes/profileRoutes.js"
 import router from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use(cors({
-    origin: 'https://assignment-project-zeta.vercel.app/',
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
